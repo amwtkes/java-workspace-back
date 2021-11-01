c79d5839caa1ec07779c0241800631236cd811f8
amwtke <amwtke@gmail.com>
2021-11-01 22:05:21 +0800

    first commit

---
+++ projects/src/test/java/com/xiaojin/projects/TmpTest.java
@@ -0,0 +1,13 @@
+package com.xiaojin.projects;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class TmpTest {
+    @Test
+    public void test() {
+        assertThat(1, is(1));
+    }
+}
