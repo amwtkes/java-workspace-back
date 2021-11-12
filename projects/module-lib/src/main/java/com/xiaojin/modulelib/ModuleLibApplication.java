8247451e02827fd197696665e857c13895ca2a12
amwtke <amwtke@gmail.com>
2021-11-12 08:26:28 +0800

    module-lib

---
+++ projects/module-lib/src/main/java/com/xiaojin/modulelib/ModuleLibApplication.java
@@ -0,0 +1,13 @@
+package com.xiaojin.modulelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleLibApplication.class, args);
+    }
+
+}
