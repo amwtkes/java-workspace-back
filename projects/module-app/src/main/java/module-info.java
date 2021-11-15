f1e07d6d5cd36d8a35d4d4dd9e542beb95a06495
amwtke <amwtke@gmail.com>
2021-11-15 10:47:52 +0800

    module-app 需要很多spring模块 手动？

--- projects/module-app/src/main/java/module-info.java
+++ projects/module-app/src/main/java/module-info.java
@@ -1,2 +1,10 @@
-module module.app {
+open module module.app {
+    requires spring.web;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
 }
+
+
--- projects/module-lib/src/main/java/module-info.java
+++ projects/module-lib/src/main/java/module-info.java
@@ -1,3 +1,6 @@
 module module.lib {
     exports com.xiaojin.lib;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires org.apache.logging.log4j;
 }
