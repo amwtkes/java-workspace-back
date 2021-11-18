99835b467d6d2577613196e78d5098b1e2bfde44
amwtke <amwtke@gmail.com>
2021-11-18 09:18:59 +0800

    ClassPathXmlApplicationContext ok!

--- spring-study/src/main/java/com/xiaojin/App.java
+++ spring-study/src/main/java/com/xiaojin/App.java
@@ -8,5 +8,10 @@ public class App {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml", "classpath:/cat-bean.xml");
         Cat myCat = (Cat) context.getBean("myCat");
         System.out.println(myCat.getAge());
+        System.out.println(myCat.getNickName());
+
+        Person person = (Person) context.getBean("myPerson");
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
     }
 }
