7221a2832c10a2ebfdb49389a293786809a7fa55
amwtke <amwtke@gmail.com>
2021-11-18 09:21:56 +0800

    spring-study=> xml first test ok!

--- spring-study/src/main/java/com/xiaojin/App.java
+++ spring-study/src/main/java/com/xiaojin/App.java
@@ -5,6 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
+        /*
+          可以加上classpath:/
+         */
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml", "classpath:/cat-bean.xml");
         Cat myCat = (Cat) context.getBean("myCat");
         System.out.println(myCat.getAge());
