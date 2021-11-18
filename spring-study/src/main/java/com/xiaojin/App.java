679c1d8a75ad963339726b9bf5beb703405d9d37
amwtke <amwtke@gmail.com>
2021-11-18 08:35:01 +0800

    first--spring-context

---
+++ spring-study/src/main/java/com/xiaojin/App.java
@@ -0,0 +1,10 @@
+package com.xiaojin;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("services.xml", "daos.xml");
+    }
+}
