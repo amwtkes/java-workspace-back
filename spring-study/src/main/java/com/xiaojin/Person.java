6fbae1dac00f9604db0396858c96590c61143107
amwtke <amwtke@gmail.com>
2021-11-18 08:55:29 +0800

    create beans.xml

---
+++ spring-study/src/main/java/com/xiaojin/Person.java
@@ -0,0 +1,13 @@
+package com.xiaojin;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private Integer age;
+}
