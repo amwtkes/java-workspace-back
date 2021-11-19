253c0c3bd06faeb60e0922a61912b2c9bfc36d0c
amwtke <amwtke@gmail.com>
2021-11-19 16:03:43 +0800

    spring-study=> two context

--- spring-study/src/main/java/com/xiaojin/App.java
+++ spring-study/src/main/java/com/xiaojin/App.java
@@ -16,5 +16,17 @@ public class App {
         Person person = (Person) context.getBean("myPerson");
         System.out.println(person.getAge());
         System.out.println(person.getName());
+
+        /**
+         * exception thrown
+         */
+//        ApplicationContext context2 = new ClassPathXmlApplicationContext("classpath:/cat-bean.xml");
+//        Cat myCat2 = (Cat) context2.getBean("myCat");
+//        System.out.println(myCat2.getAge());
+//        System.out.println(myCat2.getNickName());
+//
+//        Person person2 = (Person) context2.getBean("myPerson");
+//        System.out.println(person2.getAge());
+//        System.out.println(person2.getName());
     }
 }
