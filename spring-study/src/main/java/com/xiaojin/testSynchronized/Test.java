c4a3bf0b19fd7971d375a95db4eb4334159af880
amwtke <amwtke@gmail.com>
2021-12-30 16:06:01 +0800

    测试代码

---
+++ spring-study/src/main/java/com/xiaojin/testSynchronized/Test.java
@@ -0,0 +1,13 @@
+package com.xiaojin.testSynchronized;
+
+public class Test {
+    void n(){
+        synchronized (this){
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
