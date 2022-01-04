13778bb04105a31c1e3d3f033360296f4ab0f790
amwtke <amwtke@gmail.com>
2022-01-04 15:16:42 +0800

    AtHome->refactor - add input in context

--- projects/athome/src/main/java/com/xiaojin/algorithm/base/AlgorithmGeneralContext.java
+++ projects/athome/src/main/java/com/xiaojin/algorithm/base/AlgorithmGeneralContext.java
@@ -1,8 +1,15 @@
 package com.xiaojin.algorithm.base;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import runtime.processor.baseprocessor.ProcessorException;
 import runtime.processor.defaultprocessor.DefaultProcessorContext;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class AlgorithmGeneralContext extends DefaultProcessorContext<Object> {
+    private String input;
+
     public AlgorithmGeneralContext() {
         super();
         this.getProcessorResult().setResult(null);
@@ -11,5 +18,11 @@ public class AlgorithmGeneralContext extends DefaultProcessorContext<Object> {
     public void finish(Object object) {
         this.getProcessorResult().setResult(object);
         this.setFinished(true);
+    }
+
+    public void assertInputNotBeNull(AlgorithmGeneralContext algorithmGeneralContext) throws ProcessorException {
+        if (algorithmGeneralContext.getInput().equals("")) {
+            throw new ProcessorException("Input should not be null!");
+        }
     }
 }
--- projects/athome/src/main/java/com/xiaojin/algorithm/maxsequencingsublistsum/processors/MaxSequencingSubListSum_NaiveIntProcessor.java
+++ projects/athome/src/main/java/com/xiaojin/algorithm/maxsequencingsublistsum/processors/MaxSequencingSubListSum_NaiveIntProcessor.java
@@ -4,6 +4,7 @@ import com.xiaojin.algorithm.base.AlgorithmGeneralContext;
 import com.xiaojin.algorithm.base.AlgorithmGeneralProcessor;
 import org.springframework.stereotype.Component;
 import runtime.processor.annotation.SortOrder;
+import runtime.processor.baseprocessor.ProcessorException;
 
 import static com.xiaojin.algorithm.maxsequencingsublistsum.processors.M1ProcessorPriority.ALL;
 
@@ -11,7 +12,9 @@ import static com.xiaojin.algorithm.maxsequencingsublistsum.processors.M1Process
 @SortOrder(ALL)
 public class MaxSequencingSubListSum_NaiveIntProcessor implements AlgorithmGeneralProcessor {
     @Override
-    public void process(AlgorithmGeneralContext algorithmGeneralContext) {
+    public void process(AlgorithmGeneralContext algorithmGeneralContext) throws ProcessorException {
+        algorithmGeneralContext.assertInputNotBeNull(algorithmGeneralContext);
+
         algorithmGeneralContext.finish(101);
     }
 }
