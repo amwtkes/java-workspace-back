3877c1866277ab864f4a53ac3d9b121009797a7d
amwtke <amwtke@gmail.com>
2022-02-12 20:09:56 +0800

    AtHome->stack and queue-->加入注释

--- projects/athome/src/main/java/com/xiaojin/algorithm/datastructure/tree/CompleteBinaryTree.java
+++ projects/athome/src/main/java/com/xiaojin/algorithm/datastructure/tree/CompleteBinaryTree.java
@@ -16,6 +16,7 @@ public class CompleteBinaryTree<T> {
     private TreeNodeAction<TreeNode<T>> whenReachLastElementOfALevel;
 
     public void add(TreeNode<T> node) {
+        //第一个节点插入情况
         if (this.root == null) {
             this.root = node;
             node.setTreeNodePos(TreeNodePos.Root);
@@ -25,7 +26,7 @@ public class CompleteBinaryTree<T> {
             level++;
             return;
         }
-
+        //第二层插入情况
         if (this.currentNode == this.root) {
             if (this.root.getLeft() == null) {
                 this.root.setLeft(node);
@@ -40,7 +41,7 @@ public class CompleteBinaryTree<T> {
             level++;
             return;
         }
-
+        //如果刚插完左子树，无论什么情况都是插右子树
         if (this.currentNode.getTreeNodePos() == TreeNodePos.Left) {
             this.currentNode.getParent().setRight(node);
             node.setTreeNodePos(TreeNodePos.Right);
@@ -49,6 +50,7 @@ public class CompleteBinaryTree<T> {
             nodeCount++;
             return;
         }
+        //如果刚刚插完右子树，那么情况就复杂了
         if (this.currentNode.getTreeNodePos() == TreeNodePos.Right) {
             insertWhenPosRight(this, node);
             nodeCount++;
@@ -58,12 +60,14 @@ public class CompleteBinaryTree<T> {
     private void insertWhenPosRight(CompleteBinaryTree<T> tree, TreeNode<T> node) {
         TreeNode<T> currentNode = tree.getCurrentNode();
         TreeNode<T> tmp = currentNode.getParent();
+        //循环向上取父亲节点，判断是否是右子树或者已经到了根。这个时候就是转折，到了转折就该递归向左直到找到空位插入。
         while (tmp.getTreeNodePos() == TreeNodePos.Right && tmp != tree.getRoot()) {
             tmp = tmp.getParent();
         }
         //找到转折点，并将指针指向转折点的right
         if (tmp.getTreeNodePos() == TreeNodePos.Root) {
             tmp = tmp.getLeft();
+            //只有最右侧叶子结点才能表示是新的层。最右侧的右子树子节点会一直递归到树根。
             tree.setLevel(tree.getLevel() + 1);
         } else if (tmp.getTreeNodePos() == TreeNodePos.Left) {
             tmp = tmp.getParent().getRight();
