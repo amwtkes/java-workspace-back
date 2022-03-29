427a5279345966b3a793c74890d3b5f3cef89c3f
amwtke <amwtke@gmail.com>
2022-03-29 15:31:46 +0800

    AtHome->算法->add comments for lcs

---
+++ projects/athome/src/main/java/com/xiaojin/algorithm/LCS/LcsService.java
@@ -0,0 +1,16 @@
+package com.xiaojin.algorithm.LCS;
+
+import com.xiaojin.algorithm.LCS.base.Lcs1Processor;
+import com.xiaojin.algorithm.LCS.base.Lcs2Processor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class LcsService {
+    private final List<Lcs1Processor> lcs1Processors;
+    private final List<Lcs2Processor> lcs2Processors;
+}
