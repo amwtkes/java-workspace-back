2b903957ead572d7c367287839ab5c5ab70fa172
amwtke <amwtke@gmail.com>
2022-04-01 19:30:50 +0800

    AtHome->算法->palindrome manacher finished

--- projects/athome/src/main/java/com/xiaojin/algorithm/palindrome/manacher/ManacherContext.java
+++ projects/athome/src/main/java/com/xiaojin/algorithm/palindrome/manacher/ManacherContext.java
@@ -1,8 +1,12 @@
 package com.xiaojin.algorithm.palindrome.manacher;
 
 import com.xiaojin.algorithm.base.AlgorithmGeneralContext;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.apache.logging.log4j.util.Strings;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class ManacherContext extends AlgorithmGeneralContext {
     public ManacherContext(String dataPath) {
         if (Strings.isBlank(dataPath)) {
@@ -11,4 +15,6 @@ public class ManacherContext extends AlgorithmGeneralContext {
         }
         this.setInputDataPath(dataPath);
     }
+
+    private int[] dp;
 }
