521f3cf04d4ae98dd013093530a65f245dd32218
amwtke <amwtke@gmail.com>
2022-04-10 01:26:15 +0800

    AtHome->算法->test 数组长度

--- projects/athome/src/main/java/com/xiaojin/athome/console/App.java
+++ projects/athome/src/main/java/com/xiaojin/athome/console/App.java
@@ -8,23 +8,31 @@ import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        int length = 10;
-        ArrayList<Integer> a = new ArrayList<>(length);
-        ArrayList<Integer> b = new ArrayList<>(length);
-        for (int i = 0; i < length; i++) {
-            a.add(makeRandom(1, 20));
-            b.add(makeRandom(1, 20));
+//        int length = 10;
+//        ArrayList<Integer> a = new ArrayList<>(length);
+//        ArrayList<Integer> b = new ArrayList<>(length);
+//        for (int i = 0; i < length; i++) {
+//            a.add(makeRandom(1, 20));
+//            b.add(makeRandom(1, 20));
+//        }
+//        for (Integer e : a) {
+//            System.out.print(e + " ");
+//        }
+//        System.out.println();
+//        for (Integer e : b) {
+//            System.out.print(e + " ");
+//        }
+//        System.out.println();
+//        List<Integer> collect = b.stream().sorted().collect(Collectors.toList());
+//        collect.forEach(p -> System.out.print(p + " "));
+
+        int[][] as = new int[5][2];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 2; j++) {
+                as[i][j] = i * j;
+            }
         }
-        for (Integer e : a) {
-            System.out.print(e + " ");
-        }
-        System.out.println();
-        for (Integer e : b) {
-            System.out.print(e + " ");
-        }
-        System.out.println();
-        List<Integer> collect = b.stream().sorted().collect(Collectors.toList());
-        collect.forEach(p-> System.out.print(p+" "));
+        System.out.println(as.length);
     }
 
     public static int makeRandom(int min, int max) {
