00397fba71d6121b26e18a7edc172b712ebb5fa7
amwtke <amwtke@gmail.com>
2022-04-26 11:01:32 +0800

    AtHome->算法-二分答案 comments

--- projects/athome/src/main/java/com/xiaojin/algorithm/二分答案/电池问题/BatteryHeapComputationProcessor.java
+++ projects/athome/src/main/java/com/xiaojin/algorithm/二分答案/电池问题/BatteryHeapComputationProcessor.java
@@ -29,8 +29,13 @@ public class BatteryHeapComputationProcessor implements BatteryProcessor {
         int[] cars = new int[batteryContext.getCarsNr()];
         int ret = 0;
         while (true) {
+            //从大顶堆中取出最大的topk个电池去给车充电
             for (int i = 0; i < cars.length; i++) {
                 Integer topValue = maxTopHeap.remove();
+                /**
+                 * 一旦出现了最大值有0的情况
+                 * 表示已经不可能有电池给nr辆车充电了，就返回结果
+                 */
                 if (topValue <= 0) {
                     batteryContext.setResultNotFinish(ret);
                     return;
@@ -38,6 +43,8 @@ public class BatteryHeapComputationProcessor implements BatteryProcessor {
                 cars[i] = topValue;
             }
             ret++;
+            //过一个小时候，每个在车里的电池都会减少一格点，然后将电池扔回堆里
+            //做完一次充电
             for (int car : cars) {
                 maxTopHeap.add(car - 1);
             }
