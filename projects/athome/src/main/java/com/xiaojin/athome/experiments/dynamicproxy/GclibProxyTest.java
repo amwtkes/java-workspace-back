a988b5e7d9fc78342685e309cd272a075c961a48
amwtke <amwtke@gmail.com>
2022-05-27 23:55:03 +0800

    AtHome-> gcLib examples

--- projects/athome/src/main/java/com/xiaojin/athome/experiments/dynamicproxy/GclibProxyTest.java
+++ projects/athome/src/main/java/com/xiaojin/athome/experiments/dynamicproxy/GclibProxyTest.java
@@ -1,10 +1,14 @@
 package com.xiaojin.athome.experiments.dynamicproxy;
 
+import org.springframework.cglib.beans.BeanGenerator;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class GclibProxyTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(PersonService.class);
 //        enhancer.setCallback((FixedValue) () -> 111);
@@ -23,5 +27,16 @@ public class GclibProxyTest {
 
         System.out.println(proxy.sayHello(null));
         System.out.println(proxy.lengthOfName("null").toString());
+
+        /////////////////////////////
+        BeanGenerator beanGenerator = new BeanGenerator();
+
+        beanGenerator.addProperty("name", String.class);
+        Object myBean = beanGenerator.create();
+        Method setter = myBean.getClass().getMethod("setName", String.class);
+        setter.invoke(myBean, "some string value set by a cglib");
+
+        Method getter = myBean.getClass().getMethod("getName");
+        System.out.println(getter.invoke(myBean));
     }
 }
