5a5ff05aac69420bef0eefc34072a6d6064cfc44
amwtke <amwtke@gmail.com>
2022-06-18 12:16:07 +0800

    AtHome-> algorithm -> 堆叠问题-2

--- projects/athome/src/test/java/com/xiaojin/algorithm/dps/堆叠问题/堆叠/PipeUpServiceTest.java
+++ projects/athome/src/test/java/com/xiaojin/algorithm/dps/堆叠问题/堆叠/PipeUpServiceTest.java
@@ -1,15 +1,16 @@
 package com.xiaojin.algorithm.dps.堆叠问题.堆叠;
 
+import com.xiaojin.algorithm.dps.堆叠问题.堆叠.processors.PipeUpEndWayProcessor;
+import com.xiaojin.algorithm.dps.堆叠问题.堆叠.processors.PipeUpGreedyProcessor;
+import com.xiaojin.algorithm.dps.堆叠问题.堆叠.processors.PipeUpNaiveProcessor;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {PipeUpService.class})
+@SpringBootTest(classes = {PipeUpService.class, PipeUpNaiveProcessor.class, PipeUpEndWayProcessor.class, PipeUpGreedyProcessor.class})
 class PipeUpServiceTest {
     @Autowired
     private PipeUpService pipeUpService;
